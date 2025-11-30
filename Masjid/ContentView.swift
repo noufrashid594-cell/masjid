cb3b5b4693720c2f81eb735e64ffa5aea100268f
nouf <noufrashid594@gmail.com>
2025-11-30 09:25:06 +0300

    Initial Commit

---
+++ Masjid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Masjid
+//
+//  Created by nouf on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Masjid/MasjidApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasjidApp.swift
+//  Masjid
+//
+//  Created by nouf on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MasjidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
